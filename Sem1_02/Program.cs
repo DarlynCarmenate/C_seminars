21cb9c39c516bbcfaba0bba08d1efc9e2ad84711
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-13 11:48:14 +0200

    числа от N до -N

---
+++ Sem1_02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу. которая на вход принимает число N, 
+// а на выход отдает все числа от N до - N.
+
+Console.WriteLine("Введите число: ");
+string s_b = Console.ReadLine();
+int b = int.Parse(s_b);
+
+int a = - b;
+while(a <= b)
+{
+	Console.Write($"{a} ");
+a++;
+}
