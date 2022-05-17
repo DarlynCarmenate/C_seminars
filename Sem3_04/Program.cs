b51ce32768b9b7785bfa749560ab4c2f8a19574a
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-17 22:18:22 +0200

    Кратно 7 и 23

---
+++ Sem3_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4. Программа, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23
+
+void TwoMult(int number)
+{
+    if(number % 7 == 0 && number % 23 == 0)
+    {
+        Console.WriteLine("Yes");
+    }
+    else 
+    {
+        Console.WriteLine("No");
+    }
+}
+
+TwoMult(161);
