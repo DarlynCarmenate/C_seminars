472de927c2bb247c70c739b90fa883da799faea2
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-17 22:08:56 +0200

    Первое кратно второму

---
+++ Sem2_03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход два числа и выводит, является ли второе число кратно 
+// первому. Если не является, то программа выводит остаток от деления.
+
+void MultOrNot(int n1, int n2)
+{
+    if (n1 % n2 == 0)
+    {
+        Console.WriteLine($"{n1}, {n2} -> multiple");
+    }
+    else
+    {
+        Console.WriteLine($"{n1}, {n2} -> not multiple, the remainder -> {n1 % n2}");
+    }
+}
+
+MultOrNot(30, 5);
