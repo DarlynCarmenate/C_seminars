40e25b7da7df1167020c9fc3b29f3b54ea45a730
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-17 07:51:47 +0200

    Последняя цифра числа

---
+++ Sem2_01/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число, а на выходе
+// показывает его последнюю цифру
+
+int LastNumber(int number)
+{
+    return number % 10;
+}
+Console.WriteLine(LastNumber(395));
+
