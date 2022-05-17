4c016361c2d0477bf6579de631d661860c338669
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-17 21:30:51 +0200

    БОльшая цифра случайного числа

---
+++ Sem2_02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая выводит случайное число в диапазоне (10, 99) 
+// и показывает самую большую цифру числа
+
+int MaxNumber(int number)
+{
+    int n1 = number % 10;
+    int n2 = number / 10;
+    if (n1 > n2)
+    {
+        return n1;
+    }
+    else
+    {
+        return n2;
+    }
+}
+int rnd = new Random().Next(10, 100);
+Console.WriteLine(rnd);
+Console.WriteLine(MaxNumber(rnd));
