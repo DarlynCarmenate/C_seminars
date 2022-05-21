f0a2ca18fe26145ce7040ba10f8950426c451aa5
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-21 05:15:17 +0200

    Таблица квадратов от 1 до N

---
+++ Sem3_02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает число N и выдает таблицу квадратов от 1 до N
+
+void Squares(int num)
+{
+    double[] arr = new double[num];
+    int i = 0;
+    while (i < num)
+    {
+    arr[i] = Math.Pow((i+1), 2);
+    Console.Write($"{arr[i]}, ");    
+    i++;
+    }
+}
+
+Squares(5);
