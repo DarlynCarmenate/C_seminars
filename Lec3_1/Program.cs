1455e3f0e1260b9975fb66ddc4976d7d218913e6
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-22 16:04:00 +0200

    Печать повторного текста

---
+++ Lec3_1/Program.cs
@@ -0,0 +1,15 @@
+﻿string Method(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method(10, "t");
+Console.WriteLine(res);
