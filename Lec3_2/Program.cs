f9568693f968a32c636eb4ba7e006475466ae725
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-22 22:04:33 +0200

    Сортировка массива выбором

---
+++ Lec3_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Сортировка массива методом выбора
+
+int[] arr = {1, 4, 2, 8, 12, 5, 0, 3, 5, 1};
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minIndex]) minIndex = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+    }
+}
+
+PrintArray(arr);
+SortArray(arr);
+PrintArray(arr);
