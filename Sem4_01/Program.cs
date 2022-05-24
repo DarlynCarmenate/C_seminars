4ef56e3e921202d772d4fee0f91acf79e7d3d87e
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-24 20:37:54 +0200

    Сумма от 1 до А

---
+++ Sem4_01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int AllSum(int number)
+{
+    int sum = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine(AllSum(7));
