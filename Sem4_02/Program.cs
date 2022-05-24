0af6488e01e062f75c46fe9d68562aef72578978
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-24 20:55:42 +0200

    Количество цифр в числе

---
+++ Sem4_02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+int CountDigits(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count ++;
+    }
+    return count;
+}
+
+Console.WriteLine(CountDigits(3456777));
