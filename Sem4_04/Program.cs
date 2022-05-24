a6731225609a490b4d1814c90d8ca1437a1a768e
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-24 22:27:01 +0200

    Массив со случайными 0 и 1

---
+++ Sem4_04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями 
+// и единицами в случайном порядке.
+
+void RandomArray(int n)
+{
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+        Console.Write($"{arr[i]}, ");
+    }
+}
+
+RandomArray(8);
