3e4d9d0d2b40e899e202452bde5e6bff1d6e7e4a
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-30 21:55:12 +0200

    Замена знаков элементов массива

---
+++ Sem5_02/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу замена элементов массива: положительные элементы 
+// замените на соответствующие отрицательные, и наоборот.
+
+int[] RandArray (int length, int min, int max)
+{
+	int[] answer = new int[length];
+	for(int i = 0; i < length; i++)
+	{
+		answer[i] = new Random().Next(min, max);
+	}
+	return answer;
+}
+
+void PrintArray(int[] arr)
+{
+	int count = arr.Length;
+    Console.Write("[");
+	for(int pos = 0; pos < count - 1; pos++)
+	{
+		Console.Write($"{arr[pos]}, ");
+	}
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
+
+int[] ChangeArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = - arr[i];
+    }
+    return arr;
+}
+
+int[] array = RandArray(12, 10, 100);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
