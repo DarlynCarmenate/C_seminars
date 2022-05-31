69bd2587359687978eef349486115d67c6c789e3
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-31 19:26:42 +0200

    Существует ли треугольник

---
+++ Sem6_02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет,
+//  может ли существовать треугольник с сторонами такой длины.
+
+string TriangleCheck(int a, int b, int c)
+{
+    if(a + b > c && b + c > a && c + a > b) return "Yes";
+    return "No";
+}
+
+Console.WriteLine(TriangleCheck(4, 6, 7));
