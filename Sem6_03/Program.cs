4a5a0e864960bb34a84ecb5b3b7cb8e8dddff36b
darlyncarmenate <darlyncarmenate@gmail.com>
2022-05-31 22:48:55 +0200

    Десятичное число в двоичное

---
+++ Sem6_03/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string DecimToDouble(int num)
+{
+    string result = "";
+    while(num > 0)
+    {
+        result = num%2 + result;
+        num = num / 2;
+    }
+    return result;
+}
+
+Console.WriteLine(DecimToDouble(45));
