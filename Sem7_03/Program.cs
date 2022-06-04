43ebee3d2848e6974353fbe741d040e7ac5d2957
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-04 02:42:23 +0200

    Сумма чисел по диагонали

---
+++ Sem7_03/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+
+int[,] FillArray(int row, int column)
+{
+	int[,] array = new int[row, column];
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			array[i, j] = new Random().Next(0, 10); 
+		}
+	} 
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write($"{array[i, j]}\t"); 
+		}
+	Console.WriteLine();
+	} 
+}
+
+int Diagonal(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			if(i == j) result += array[i, j];
+		}
+	} 
+    return result;
+}
+
+int[,] array = FillArray(5, 5);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(Diagonal(array));
+
+
+
+
