b928300f36ea56d7752c0a18b70023553257b794
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-10 23:04:00 +0200

    А в степени B - рекурсия

---
+++ Sem9_04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и возводит число А 
+// в целую степень B с помощью рекурсии.
+
+int ExponentNum(int a, int b)
+{
+    if(b == 0) return 1;
+    return ExponentNum(a, (b - 1)) * a;
+}
+
+Console.Write(ExponentNum(2, 5));
