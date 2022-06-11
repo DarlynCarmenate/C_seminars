b01775d93d6cfb56670f929491d054b8f2941f3b
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-11 16:17:43 +0200

    Цифры числа в обратном порядке - рекурсия

---
+++ Sem10_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано натуральное число N. Выведите все егоцифры в обратном порядке по одному.
+
+void NumsVersa(int n)
+{
+    if(n == 0) return;
+    Console.Write(n % 10);
+    NumsVersa(n/10);
+}
+
+NumsVersa(123);
