88938a751fc6ff0e53da657f3b1f3b7a953a4785
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-15 14:48:23 +0200

    Строка от а до b - без рекурсии

---
+++ Lec7_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Сoберите строку с числами от a до b, a <=b
+
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+Console.Write(NumbersFor(1, 10));
+
