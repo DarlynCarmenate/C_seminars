6e9b97ca1fd7934898f2db23880ba5d051212b5a
darlyncarmenate <darlyncarmenate@gmail.com>
2022-06-15 14:49:08 +0200

    Строка от а до b - рекурсия

---
+++ Lec7_01_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Сoберите строку с числами от a до b, a <=b
+
+string NumberRec(int a, int b)
+{
+    if(a <= b) return $"{a} {NumberRec(a + 1, b)}";
+    else return string.Empty;
+}
+
+Console.WriteLine(NumberRec(1, 10));
